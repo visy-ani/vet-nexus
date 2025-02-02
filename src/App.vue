657250590a444511ccec0cf3129f21c60ce53b04
chocoanish <chocoboyanish566@gmail.com>
2025-02-02 16:40:44 +0530

    refactor App.vue; simplify navbar styles and remove unused scroll logic

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="min-h-screen">
     <!-- Navbar -->
-    <nav :class="[
-      'navbar fixed w-full z-50 transition-all duration-300',
-      isScrolled ? 'py-4 bg-[#2D3250] shadow-lg' : 'py-12'
-    ]">
+    <nav class="navbar fixed w-full z-50 py-4 bg-[#2D3250] shadow-lg transition-all duration-300">
       <div class="navcontainer mx-auto flex justify-between items-start px-4">
         <!-- Logo and Know image aligned left -->
         <div class="flex flex-col items-start">
@@ -15,7 +12,7 @@
         <!-- Mobile Menu Button -->
         <button @click="toggleMenu" class="block lg:hidden mt-6">
           <svg xmlns="http://www.w3.org/MPH" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-            :class="[isScrolled ? 'text-black' : 'text-white']" class="w-6 h-6">
+            :class="['text-white']" class="w-6 h-6">
             <path v-if="!isMenuOpen" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               d="M4 6h16M4 12h16M4 18h16" />
             <path v-else stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -35,7 +32,7 @@
         <!-- Mobile/Tablet Menu -->
         <div v-show="isMenuOpen"
           class="lg:hidden absolute top-full left-0 w-full bg-white/95 backdrop-blur-md shadow-lg">
-          <div class="flex flex-col py-4">
+          <div class="flex flex-col py-4 bg-[#2D3250]">
             <a v-for="link in navLinks" :key="link" href="#"
               class="px-6 py-3 text-white hover:bg-gray-100 text-lg border-b border-gray-100 last:border-none"
               @click="closeMenu">
@@ -313,11 +310,11 @@ export default {
     handleSubmit() {
       console.log('Form submitted:', this.formData)
     },
-    handleScroll() {
-      const heroHeight = window.innerHeight; 
-      const scrollTriggerPoint = heroHeight / 5;
-      this.isScrolled = window.scrollY > scrollTriggerPoint;
-    },
+    // handleScroll() {
+    //   const heroHeight = window.innerHeight; 
+    //   const scrollTriggerPoint = heroHeight / 5;
+    //   this.isScrolled = window.scrollY > scrollTriggerPoint;
+    // },
     toggleMenu() {
       this.isMenuOpen = !this.isMenuOpen;
     },
